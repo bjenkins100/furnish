8f447f796fb46278875f291b15dcab1168bd2884
Erik Hollensbe <erik+github@hollensbe.org>
2013-02-12 12:41:14 -0800

    first stab at a standalone logger

---
+++ lib/furnish/logger.rb
@@ -0,0 +1,25 @@
+require 'logger'
+
+module Furnish
+  class Logger
+    attr_accessor :debug_level
+
+    def initialize(logger_io=$stderr, debug_level=0)
+      @io = logger_io
+      @io.sync = true
+      @debug_level = debug_level
+    end
+
+    def if_debug(level=1, else_block=nil, &block)
+      if level >= debug_level and block
+        instance_eval(&block)
+      elsif else_block
+        instance_eval(&else_block)
+      end
+    end
+
+    def method_missing(sym, *args)
+      @io.__send__(sym, *args)
+    end
+  end
+end
