027a0812479a466f6678f8ffa764961e3c8a3b7b
Erik Hollensbe <erik+github@hollensbe.org>
2013-02-12 12:30:40 -0800

    basic VM tests.

---
+++ test/test_vm.rb
@@ -0,0 +1,17 @@
+require 'helper'
+
+class TestVM < Furnish::TestCase
+  def test_initialize
+    vm = Furnish::VM.new
+    kinds = {
+      :groups       => Furnish::VMGroup,
+      :dependencies => Furnish::VMGroup,
+      :provisioned  => Palsy::Set,
+      :working      => Palsy::Set
+    }
+
+    kinds.each do |key, klass|
+      assert_kind_of(klass, vm.send(key))
+    end
+  end
+end
