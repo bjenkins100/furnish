06535757dd6e1f14bdb314ae0d12e514ce48f0c6
Erik Hollensbe <erik+github@hollensbe.org>
2013-02-12 12:25:12 -0800

    small furnish test harness in helper

--- test/helper.rb
+++ test/helper.rb
@@ -1,2 +1,30 @@
+require 'bundler/setup'
 require 'minitest/unit'
+require 'furnish'
+require 'tempfile'
+
+module Furnish
+  class TestCase < MiniTest::Unit::TestCase
+
+    def reinit
+      @tempfiles ||= []
+      file = Tempfile.new('furnish_db')
+      @tempfiles.push(file)
+      Furnish.init(file.path)
+      return file
+    end
+
+    def setup
+      reinit
+    end
+
+    def teardown
+      Furnish.shutdown
+      @tempfiles.each do |file|
+        file.unlink
+      end
+    end
+  end
+end
+
 require 'minitest/autorun'
