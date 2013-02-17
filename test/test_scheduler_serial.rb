aea238766145c2da635d595365df9c62efb787bc
Erik Hollensbe <erik+github@hollensbe.org>
2013-02-16 19:48:34 -0700

    refactor asserting the scheduler state into some methods

--- test/test_scheduler_serial.rb
+++ test/test_scheduler_serial.rb
@@ -7,6 +7,17 @@ class TestSchedulerSerial < Furnish::SchedulerTestCase
     @sched.serial = true
   end
 
+  def assert_started(name)
+    assert_includes(@sched.solved, name, 'scheduler thinks it solved it')
+    assert(@sched.vm_groups[name].first.store[ [name, "startup"].join("-") ], "dummy provisioner for #{name} recorded the startup run")
+    refute(@sched.vm_groups[name].first.store[ [name, "shutdown"].join("-") ], "dummy provisioner for #{name} has not recorded the shutdown run")
+  end
+
+  def assert_shutdown(name, provisioner)
+    refute_includes(@sched.solved, name, 'scheduler thinks it solved it')
+    assert(provisioner.store[ [name, "shutdown"].join("-") ], "dummy provisioner for #{name} recorded the shutdown run")
+  end
+
   def test_provision_cycle
     machine_names = %w[blarg blarg2 blarg3]
 
@@ -17,9 +28,7 @@ class TestSchedulerSerial < Furnish::SchedulerTestCase
     @sched.run
 
     machine_names.each do |name|
-      assert_includes(@sched.solved, name, 'scheduler thinks it solved it')
-      assert(@sched.vm_groups[name].first.store[ [name, "startup"].join("-") ], "dummy provisioner for #{name} recorded the startup run")
-      refute(@sched.vm_groups[name].first.store[ [name, "shutdown"].join("-") ], "dummy provisioner for #{name} has not recorded the shutdown run")
+      assert_started(name)
     end
 
     machine_provs = machine_names.map { |n| @sched.vm_groups[n].first }
@@ -27,8 +36,7 @@ class TestSchedulerSerial < Furnish::SchedulerTestCase
     @sched.teardown
 
     machine_names.each_with_index do |name, i|
-      refute_includes(@sched.solved, name, 'scheduler thinks it solved it')
-      assert(machine_provs[i].store[ [name, "shutdown"].join("-") ], "dummy provisioner for #{name} recorded the shutdown run")
+      assert_shutdown(name, machine_provs[i])
     end
   end
 end
