c5da49776b98e10b865574ea696634b8868da43f
Erik Hollensbe <erik+github@hollensbe.org>
2013-02-17 11:34:19 -0700

    mutex wrap all i/o that goes through the logger.

--- lib/furnish/logger.rb
+++ lib/furnish/logger.rb
@@ -1,4 +1,5 @@
 require 'logger'
+require 'thread'
 
 module Furnish
   class Logger
@@ -6,22 +7,25 @@ module Furnish
     attr_reader   :io
 
     def initialize(logger_io=$stderr, debug_level=0)
+      @write_mutex = Mutex.new
       @io = logger_io
       @io.sync = true
       @debug_level = debug_level
     end
 
     def if_debug(level=1, else_block=nil, &block)
-      if debug_level >= level and block
-        io.instance_eval(&block)
-      elsif else_block
-        io.instance_eval(&else_block)
+      @write_mutex.synchronize do
+        if debug_level >= level and block
+          io.instance_eval(&block)
+        elsif else_block
+          io.instance_eval(&else_block)
+        end
       end
     end
 
     def method_missing(sym, *args)
       raise NoMethodError, "#{io.inspect} has no method #{sym}" unless io.respond_to?(sym)
-      io.__send__(sym, *args)
+      @write_mutex.synchronize { io.__send__(sym, *args) }
     end
   end
 end
