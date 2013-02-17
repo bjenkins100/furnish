36ad60ac7b3cac74a17ff06103d749c7710a4847
Erik Hollensbe <erik+github@hollensbe.org>
2013-02-17 09:19:43 -0700

    test signal handler as it matters to serial scheduler

--- test/test_scheduler_serial.rb
+++ test/test_scheduler_serial.rb
@@ -128,4 +128,48 @@ class TestSchedulerSerial < Furnish::SchedulerTestCase
     assert_nil(sched.wait_for('blarg'))
     sched.stop # does not explode
   end
+
+  def test_run_arguments
+    tempfiles = []
+
+    clear_signal = lambda do |sig|
+      Signal.trap(sig) { nil } if Signal.list[sig]
+    end
+
+    signals = %w[INFO USR2]
+
+    # flush any previous scheduler runs which might have set signals with null
+    # handlers
+    signals.each(&clear_signal)
+
+    signals.each do |signal|
+      clear_signal.call(signal)
+
+      if Signal.list[signal] # not everyone has INFO
+        tf = Tempfile.new('furnish_signal_handlers')
+        tempfiles.push(tf)
+        Furnish.logger = Furnish::Logger.new(tf)
+
+        @sched.run(false)
+        Process.kill(signal, Process.pid)
+
+        sleep 0.1 # wait for any writes to complete
+
+        %w[solved working waiting].each do |section|
+          refute_match(/#{section}/, File.read(tf.path), "#{signal} yielded no output with the #{section} set")
+        end
+
+        @sched.run(true)
+        Process.kill(signal, Process.pid)
+
+        sleep 0.1 # wait for any writes to complete
+
+        %w[solved working waiting].each do |section|
+          assert_match(/#{section}/, File.read(tf.path), "#{signal} yielded output with the #{section} set")
+        end
+      end
+    end
+  ensure
+    tempfiles.each { |f| f.unlink }
+  end
 end
