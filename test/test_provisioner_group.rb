dea42dae8305dd6fd7299cd458abd13e10e285b4
Erik Hollensbe <erik+github@hollensbe.org>
2013-02-23 00:08:21 -0800

    tests for provisioner groups.

---
+++ test/test_provisioner_group.rb
@@ -0,0 +1,40 @@
+require 'helper'
+
+class TestVMGroup < Furnish::TestCase
+  def test_constructor
+    dummy = Dummy.new
+    pg = Furnish::ProvisionerGroup.new(dummy, 'blarg')
+    assert_includes(pg, dummy)
+    assert_equal('blarg', pg.name)
+    assert_kind_of(Array, pg.dependencies)
+    assert_empty(pg.dependencies)
+    assert_equal('blarg', dummy.name)
+
+    dummy = Dummy.new
+    pg = Furnish::ProvisionerGroup.new([dummy], 'blarg2', %w[blarg])
+    assert_includes(pg, dummy)
+    assert_equal('blarg2', pg.name)
+    assert_equal(['blarg'], pg.dependencies)
+    assert_equal('blarg2', dummy.name)
+  end
+
+  def test_up_down
+    store = Palsy::Object.new('dummy')
+    dummy = Dummy.new
+    pg = Furnish::ProvisionerGroup.new(dummy, 'blarg')
+
+    assert(pg.startup, 'started')
+    assert(store[ [pg.name, 'startup'].join("-") ], 'startup ran')
+    assert(pg.shutdown, 'stopped')
+    assert(store[ [pg.name, 'startup'].join("-") ], 'shutdown ran')
+
+    dummy = StartFailDummy.new
+    pg = Furnish::ProvisionerGroup.new(dummy, 'blarg2')
+    assert_raises(RuntimeError, "Could not provision #{pg.name} with provisioner #{dummy.class.name}") { pg.startup }
+
+    dummy = StopFailDummy.new
+    pg = Furnish::ProvisionerGroup.new(dummy, 'blarg3')
+    assert_raises(RuntimeError, "Could not deprovision #{pg.name}/#{dummy.class.name}") { pg.shutdown }
+    pg.shutdown(true)
+  end
+end
