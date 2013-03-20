5f2e6d7291ced11bf57cb5a209e4a717bd0758ca
Erik Hollensbe <erik+github@hollensbe.org>
2013-03-20 09:31:50 -0700

    documentation for Furnish::TestCase

--- lib/furnish/test.rb
+++ lib/furnish/test.rb
@@ -3,8 +3,19 @@ require 'tempfile'
 require 'furnish'
 
 module Furnish
+  #
+  # Furnish::TestCase is a test harness for testing things with furnish, like
+  # provisioner libraries. It is intended to be consumed by other libraries.
+  #
+  # There are few others, such as SchedulerTestCase and
+  # RunningSchedulerTestCase which are tuned to specific scenarios, but inherit
+  # from this class.
+  #
+  # The basic case initializes furnish and the logger in a safe way in setup,
+  # and cleans up in teardown.
+  #
   class TestCase < MiniTest::Unit::TestCase
-    def setup
+    def setup # :nodoc:
       @tempfiles ||= []
       file = Tempfile.new('furnish_db')
       @tempfiles.push(file)
@@ -15,7 +26,7 @@ module Furnish
       return file
     end
 
-    def teardown
+    def teardown # :nodoc:
       Furnish.logger.close
       Furnish.shutdown
       @tempfiles.each do |file|
@@ -24,37 +35,62 @@ module Furnish
     end
   end
 
+  #
+  # SchedulerTestCase inherits from Furnish::TestCase and configures a threaded
+  # scheduler, but does not attempt to start it. It's intended to be a
+  # primitive for cases where you might create a number of schedulers.
+  #
+  # If the scheduler throws an exception for any reason, the test suite will
+  # abort.
+  #
+  # RunningSchedulerTestCase deals with managing a running scheduler for you.
+  #
   class SchedulerTestCase < TestCase
+    ##
+    # Furnish::Scheduler object.
     attr_reader :sched
 
-    def setup
+    def setup # :nodoc:
       super
       @sched = Furnish::Scheduler.new
       @monitor = Thread.new { loop { @sched.running?; sleep 1 } }
       @monitor.abort_on_exception = true
     end
 
-    def teardown
+    def teardown # :nodoc:
       @monitor.kill rescue nil
       super
     end
 
+    ##
+    #
+    # Assert the named group is solved, as far as the scheduler's concerned.
+    #
     def assert_solved(name)
       assert_includes(sched.vm.solved, name, "#{name} is solved in the scheduler")
     end
 
+    ##
+    #
+    # Refute the named group is solved, as far as the scheduler's concerned.
+    #
     def refute_solved(name)
       refute_includes(sched.vm.solved, name, "#{name} is solved in the scheduler")
     end
   end
 
+  ##
+  #
+  # Inherits from SchedulerTestCase and manages a running scheduler in
+  # conjunction with all the other features.
+  #
   class RunningSchedulerTestCase < SchedulerTestCase
-    def setup
+    def setup # :nodoc:
       super
       @sched.run
     end
 
-    def teardown
+    def teardown # :nodoc:
       @sched.stop
       sleep 0.3 while @sched.running?
       super
