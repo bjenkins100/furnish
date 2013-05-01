946ae907c7e740ec7dcb1e3ef6aad5fe6afeb81b
Erik Hollensbe <erik+github@hollensbe.org>
2013-05-01 03:00:06 -0700

    documentation

--- lib/furnish/provisioners/dummy_vm.rb
+++ lib/furnish/provisioners/dummy_vm.rb
@@ -1,8 +1,15 @@
 require 'furnish/provisioners/dummy_include'
 require 'furnish/provisioners/vm'
 
-module Furnish
-  module Provisioner
+module Furnish # :nodoc:
+  module Provisioner # :nodoc:
+    #
+    # Primarily for testing, this is a provisioner that has a basic storage
+    # model.
+    #
+    # In short, unless you're writing tests you should probably never use this
+    # code.
+    #
     class DummyVM < VM
       include Furnish::Provisioner::DummyInclude
     end
--- lib/furnish/provisioners/vm.rb
+++ lib/furnish/provisioners/vm.rb
@@ -2,13 +2,32 @@ require 'furnish/provisioners/api'
 
 module Furnish # :nodoc:
   module Provisioner # :nodoc:
+    #
+    # The VM provisioner API (which inherits from Furnish::Provisioner::API)
+    # just adds a few convenience functions and a tracking database for VM
+    # work. This makes it easy to get the data back you need and an easy,
+    # consistent way to stuff it away when you have data to store.
+    #
+    # See the methods themselves for more information.
+    #
     class VM < API
+      #
+      # Our database, which won't be available until the provisioner is added
+      # to a ProvisionerGroup.
+      #
       attr_reader :vm
 
+      #
+      # Hook required by Furnish::ProvisionerGroup#run_add_hook. See
+      # Furnish::Provisioner::API#added_to_group for more information.
+      #
       def added_to_group
         @vm = Palsy::Map.new(self.class.name.gsub(/::/, '_').downcase + "_vms", furnish_group_name)
       end
 
+      #
+      # Add a vm. Takes a name (String) and metadata (Hash).
+      #
       def add_vm(name, metadata)
         unless vm
           raise "Cannot add machine '#{name}' to ungrouped provisioner"
@@ -21,6 +40,9 @@ module Furnish # :nodoc:
         vm[name] = metadata
       end
 
+      #
+      # Delete a VM. Takes a name (String).
+      #
       def remove_vm(name)
         unless vm
           raise "Cannot delete machine '#{name}' from ungrouped provisioner"
@@ -29,6 +51,10 @@ module Furnish # :nodoc:
         vm.delete(name)
       end
 
+      #
+      # List the VMs. If the provisioner is not named (a part of a
+      # Furnish::ProvisionerGroup), will return an empty array.
+      #
       def list_vms
         return [] unless vm
         return vm.keys
